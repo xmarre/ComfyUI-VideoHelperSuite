8eacd7c89fb965ca62e971e385deeaac3a65eb6d
xmarre <54859656+xmarre@users.noreply.github.com>
2026-08-04 10:09:51 +0200

    Register the hardened Video Combine implementation

--- __init__.py
+++ __init__.py
@@ -1,8 +1,11 @@
 from .videohelpersuite.nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .videohelpersuite.safe_video_combine import SafeVideoCombine
 import folder_paths
 from .videohelpersuite.server import server
 from .videohelpersuite import documentation
 from .videohelpersuite import latent_preview
+
+NODE_CLASS_MAPPINGS["VHS_VideoCombine"] = SafeVideoCombine
 
 WEB_DIRECTORY = "./web"
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
