9073c66eb627709be95fd01ba0878f482cc9a801
xmarre <54859656+xmarre@users.noreply.github.com>
2026-08-18 09:33:08 +0200

    Restore LoadAudioUpload category

--- videohelpersuite/nodes.py
+++ videohelpersuite/nodes.py
@@ -800,7 +800,7 @@ class LoadAudioUpload:
                      },
                 }
 
-    CATEGORY = "Video Helper Suite 🎥🅥🅗🅢"
+    CATEGORY = "Video Helper Suite 🎥🅥🅗🅢/audio"
 
     RETURN_TYPES = ("AUDIO", "FLOAT")
     RETURN_NAMES = ("audio", "duration")
@@ -1216,7 +1216,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "VHS_MergeImages": "Merge Images 🎥🅥🅗🅢",
     "VHS_MergeMasks": "Merge Masks 🎥🅥🅗🅢",
     "VHS_GetLatentCount": "Get Latent Count 🎥🅥🅗🅢",
-    "VHS_GetImageCount": "Get Image Count 🎥🅥🅗🅢",
+    "VHS_GetImageCount": "Get Image Count 🎥🅥🅗🅗🅢",
     "VHS_GetMaskCount": "Get Mask Count 🎥🅥🅗🅢",
     "VHS_DuplicateLatents": "Repeat Latents 🎥🅥🅗🅢",
     "VHS_DuplicateImages": "Repeat Images 🎥🅥🅗🅢",
